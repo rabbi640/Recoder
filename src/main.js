7eae3d3458e4073e78d1ec1c63cec3bbf29670a4
rabbi640 <mdmahfuz640@gmail.com>
2023-09-07 12:15:13 +0600

    home page update

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import "./sass/style.scss";
 import "./../node_modules/bootstrap/dist/js/bootstrap.min";
-// import { createPopper } from "@popperjs/core";
+import { createPopper } from "@popperjs/core";
 import { animateTyping } from "./Animations.js";
 
 const arr = [
-  "print('Hello, programmer')",
-  'System.out.println("Hello, World")',
-  'console.log("Hello, World")',
+  "print('Hello, Programmer')",
+  'System.out.println("Welcome to programmeing world")',
+  'console.log("How are you Programmer")',
   "SELECT 'Hello, World' FROM DUAL",
   'printf("Hello, World")',
 ];
 animateTyping("#typing", arr);
 document.querySelector(".typed-cursor").style.color = "white";
+
+
